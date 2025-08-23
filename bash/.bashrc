1f0f53b3d3eadd9bae28f6d2962bb834daff1b58
barrrguzin <sunickiy0012@mail.ru>
2025-08-23 23:54:06 +0300

    add code-flags

--- bash/.bashrc
+++ bash/.bashrc
@@ -1,6 +1,8 @@
 #!/bin/bash
 [[ $- != *i* ]] && return
 
+ELECTRON_FLAGS="--enable-features=WaylandWindowDecorations --ozone-platform-hint=auto"
+
 ### Aliases
 #
 alias ls='ls --color=auto -lh'
@@ -10,6 +12,9 @@ alias mc='mc -S ~/.config/mc/modarin256root-defbg.ini'
 #
 alias proton="~/.local/share/Steam/steamapps/common/Proton\ 9.0\ \(Beta\)/proton run"
 alias guitar_pro='wine ~/.wine/drive_c/Program\ Files/Arobas\ Music/Guitar\ Pro\ 8/GuitarPro.exe'
-
+#
+#alias chromium='chromium $ELECTRON_FLAGS'
+#alias code='code $ELECTRON_FLAGS'
+#alias slack='slack $ELECTRON_FLAGS'
 ###
 PS1='[\u@\h \W]\$ '
--- bash/autostart.sh
+++ bash/autostart.sh
@@ -1,8 +1,39 @@
 #!/bin/bash
-echo 1 > /sys/devices/platform/asus-nb-wmi/throttle_thermal_policy
-echo 80 > /sys/class/power_supply/BAT1/charge_control_end_threshold
+
+echo 1 | sudo tee /sys/devices/platform/asus-nb-wmi/throttle_thermal_policy
+echo 80 | sudo tee /sys/class/power_supply/BAT1/charge_control_end_threshold
 
 ln -s .config/bash/.bash_profile .bash_profile
 ln -s .config/bash/.bashrc .bashrc
 
+ELECTRON_FLAGS="--enable-features=WaylandWindowDecorations --ozone-platform-hint=auto"
+MARK_W="thisfileshouldpassthroughwaylandsupportargumentsaboba"
+POSTFIX_W=".bakw"
 
+pass_wayland_flags_to_electron_apps() {
+  for arg in "$@"; do
+    if ! grep --binary-files=without-match -q $MARK_W $arg; then
+        sudo rm -f "$arg$POSTFIX_W"
+        sudo cp "$arg" "$arg$POSTFIX_W"
+	sudo rm -f "$arg"
+	echo -e "#!/bin/bash\n$MARK_W\n$arg$POSTFIX_W $ELECTRON_FLAGS" | sudo tee "$arg"
+	sudo chmod +x $arg
+    fi
+  done
+}
+
+restore_wayland_flags_to_electron_apps() {
+  for arg in "$@"; do
+    if [ -e "$arg$POSTFIX_W" ]; then
+      sudo mv $arg$POSTFIX_W $arg
+    fi
+  done  
+}
+
+apps=( "/usr/bin/chromium" )
+
+if [ "$XDG_SESSION_TYPE" = "wayland" ] || [ -n "$WAYLAND_DISPLAY" ]; then
+  pass_wayland_flags_to_electron_apps "${apps[@]}"
+else
+  restore_wayland_flags_to_electron_apps "${apps[@]}"
+fi
