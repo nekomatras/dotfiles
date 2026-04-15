e6cba8e075548f142e0a37f84bc928424e3d3974
barrrguzin <sunickiy0012@mail.ru>
2026-04-15 06:53:58 +0300

    update bar

--- waybar/scripts/spotify.sh
+++ waybar/scripts/spotify.sh
@@ -2,10 +2,14 @@
 
 status=$(playerctl --player=spotify status 2>/dev/null)
 
-if [[ "$status" == "Playing" ]] || [[ "$status" == "Paused" ]]; then
+if [[ "$status" == "Playing" ]]; then
     artist=$(playerctl --player=spotify metadata artist)
     title=$(playerctl --player=spotify metadata title)
-    echo "$artist - $title"
+    echo "{\"text\": \"$artist - $title\", \"alt\": \"play\"}"
+elif [ "$status" = "Paused" ]; then
+    artist=$(playerctl --player=spotify metadata artist)
+    title=$(playerctl --player=spotify metadata title)
+    echo "{\"text\": \"$artist - $title\", \"alt\": \"pause\"}"
 else
-    echo " "
+    echo '{"text": ""}'
 fi
